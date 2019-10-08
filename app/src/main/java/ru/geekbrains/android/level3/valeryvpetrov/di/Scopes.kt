a56091ec72416e03b57f2d4e614a44e3e211675b
Valery <valera071998@gmail.com>
2019-10-08 22:27:42 +0600

    add dependency scopes

---
+++ app/src/main/java/ru/geekbrains/android/level3/valeryvpetrov/di/Scopes.kt
@@ -0,0 +1,11 @@
+package ru.geekbrains.android.level3.valeryvpetrov.di
+
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class UseCaseScope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ScreenScope
